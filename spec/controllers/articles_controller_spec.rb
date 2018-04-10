6d2f374e57710b4a516e319ad24c0deb32643c05
Vladimir Kireyko <v.kireyko@amgrade.com>
2018-04-10 12:11:58 +0300

    Add tests for dashboard and article controllers in user path

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
+  context 'logged user' do
+    render_views
 
+    before do
+      sign_in build(:user)
+      @articles = create_list(:article, 5)
+    end
+
+    describe 'GET #index' do
+      it 'return success response' do
+        get :index
+        expect(response).to have_http_status :success
+        expect(response.body).to match @articles.first.title
+      end
+    end
+
+    describe 'GET #show' do
+      it 'should return success response' do
+        article = @articles.first
+
+        get :show, params: { id: article.id }
+        expect(response).to have_http_status :success
+        expect(response.body).to match article.title
+      end
+    end
+
+  end
 end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller do
 
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns http success for auth user' do
+      sign_in build(:user)
+
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http redirect for non-auth user' do
+      get :index
+      expect(response).to have_http_status(:redirect)
+      assert_redirected_to '/login'
     end
   end
 
