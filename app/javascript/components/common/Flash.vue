50411cf28ff6b034285134da2ff61cb895b57f91
Vladimir Kireyko <v.kireyko@amgrade.com>
2018-04-13 09:45:52 +0300

    Add status map to flash component.

--- app/javascript/components/common/Flash.vue
+++ app/javascript/components/common/Flash.vue
@@ -15,13 +15,30 @@
       }
     },
 
+    data() {
+      return {
+        statusMap: {
+          notice: 'info',
+          alert: 'warning'
+        }
+      }
+    },
+
+    computed: {
+      mappedStatus() {
+        return this.statusMap.hasOwnProperty(this.status)
+          ? this.statusMap[this.status]
+          : this.status
+      }
+    },
+
     created() {
       $.toast({
-        heading: _.upperFirst(this.status),
+        heading: _.upperFirst(this.mappedStatus),
         text: this.message,
         position: 'top-right',
         loaderBg: '#ff6849',
-        icon: this.status,
+        icon: this.mappedStatus,
         hideAfter: 5000,
         stack: 3,
         afterHidden: () => this.$destroy(),
